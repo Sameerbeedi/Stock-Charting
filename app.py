4e2a995efefb22b98e409b0d41d4354cea1e6d9b
Sameerbeedi <beedi.sameer@gmail.com>
2025-05-22 20:02:41 +0530

    app-changes

--- app.py
+++ app.py
@@ -5,20 +5,20 @@ from chatbot import get_bot_response
 import os
 st.set_page_config(layout='wide', page_title="Stock Dashboard with AI Chat")
 
-#df = pd.read_csv("data/stock_data.csv", parse_dates=["timestamp"])
+df = pd.read_csv("data/stock_data.csv")
 
 print(os.path.exists("data/stock_data.csv"))  # Should print True
 
 tab1, tab2 = st.tabs(["📊 Chart", "🤖 AI Chatbot"])
 
-# with tab1:
-#     st.title("Candlestick Chart with Markers and Bands")
-#     fig = plot_candlestick(df)
-#     st.plotly_chart(fig, use_container_width=True)
+with tab1:
+    st.title("Candlestick Chart with Markers and Bands")
+    fig = plot_candlestick(df)
+    st.plotly_chart(fig, use_container_width=True)
 
-# with tab2:
-#     st.title("Ask AI about Stock Data")
-#     user_input = st.text_input("Ask me anything about TSLA stock trends...")
-#     if user_input:
-#         response = get_bot_response(df, user_input)
-#         st.write(response)
+with tab2:
+    st.title("Ask AI about Stock Data")
+    user_input = st.text_input("Ask me anything about TSLA stock trends...")
+    if user_input:
+        response = get_bot_response(df, user_input)
+        st.write(response)
