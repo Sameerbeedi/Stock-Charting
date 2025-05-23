6619193743060762d4da825f8289fe432c0990ba
Sameerbeedi <beedi.sameer@gmail.com>
2025-05-23 12:24:20 +0530

    Corrected attributes

--- animation.py
+++ animation.py
@@ -8,9 +8,9 @@ def animate_chart(df):
     def update(num):
         ax.clear()
         data = df.iloc[:num+1]
-        ax.plot(data['Date'], data['Close'], color='black')
-        ax.fill_between(data['Date'], data['Low'], data['High'], alpha=0.3)
-        ax.set_title(f"TSLA Candles up to {data['Date'].iloc[-1].date()}")
+        ax.plot(data['timestamp'], data['close'], color='black')
+        ax.fill_between(data['timestamp'], data['low'], data['high'], alpha=0.3)
+        ax.set_title(f"TSLA Candles up to {data['timestamp'].iloc[-1].date()}")
 
     ani = animation.FuncAnimation(fig, update, frames=len(df), repeat=False)
     ani.save("candlestick_animation.mp4", fps=10)
